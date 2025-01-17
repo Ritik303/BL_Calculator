93c308fa22578842a5a1d25ca0f18f8122d67069
Ritik303 <rtk3032002@gmail.com>
2025-01-17 12:02:56 +0530

    subtraction feature

---
+++ subtraction.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class subtraction {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number");
+        int n1=sc.nextInt();
+        System.out.println("Enter second number");
+        int n2=sc.nextInt();
+        System.out.println("Enter operation");
+        char ch=sc.next().charAt(0);
+        if(ch=='-'){
+        System.out.println(n1-n2);
+        }
+    }
+}
